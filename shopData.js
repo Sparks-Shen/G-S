5406f6ccfeb7f99415c8e84151c0253d4b833c1a
Sparks-Shen <2589755363@qq.com>
2026-09-09 13:18:12 +0800

    init: 上传地图页面全部代码

---
+++ shopData.js
@@ -0,0 +1,17 @@
+// 店铺巡礼点位配置
+const placeList = [
+  {
+    lng: 121.410580,
+    lat: 31.218303,
+    name:"乔氏·品绅台球（长宁来福士店）",
+    img:"assets/img/长宁来福士台球.jpg",
+    link:"places/长宁来福士台球.html"
+  },
+  {
+    lng:121.501587,
+    lat:31.235787,
+    name:"上海中心大厦商铺",
+    img:"assets/img/shop.jpg",
+    link:"places/shop1.html"
+  }
+];
