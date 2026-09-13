4855b6ef5fa51909a5542b3191073635125a5dec
Spark-Shen <2589755363@qq.com>
2026-09-14 00:20:08 +0800

    Update shopData.js

--- shopData.js
+++ shopData.js
@@ -10,13 +10,6 @@ const placeList = [
     img:"assets/img/长宁来福士台球.jpg",
     link:"places/长宁来福士台球.html"
   },
-  {
-    lng:121.501587,
-    lat:31.235787,
-    name:"上海中心大厦商铺",
-    img:"assets/img/shop.jpg",
-    link:"places/shop1.html"
-  },
   { lng: 121.410456, lat: 31.218904, name:"大寶口福·海鲜排档（长宁来福士店）", img:"assets/img/大寶口福·海鲜排档（长宁来福士店）.png", link:"places/大寶口福·海鲜排档（长宁来福士店）.html" },
 
   { lng: 121.498529, lat: 31.238028, name:"百丽宫影城（国金中心店）", img:"assets/img/百丽宫ifc.jpg", link:"places/百丽宫影城（国金中心店）.html" },
